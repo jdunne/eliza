16627cf98257c8f3c97545719581191ac698ec9d
John Dunne <johntdunne@gmail.com>
2012-11-24 18:59:05 -0500

    Refactoring.

--- eliza.py
+++ eliza.py
@@ -1,6 +1,25 @@
 import os
 import random
 
+responses = [
+    ('james', 'What is your favorite thing about James?'),
+    ('mommy', 'What color does mommy like the best?'),
+    ('daddy', 'What food does daddy like to eat the most?'),
+    ('no', 'Tell me.  Why not?'),
+    ('you', 'We are talking about you. Not me.'),
+    ('I', 'Do you always talk about yourself so much?'),
+    ('me', 'Do you always talk about yourself so much?')
+]
+ 
+random_responses = [
+    'Does my head smells like poop?',
+    'Anyone like tennis?',
+    'Is Mommy is a silly goose',
+    'What do you want to talk about?',
+    'What is your favorite game?'
+    'What movie do you like the most?'
+]
+
 def say(message):
     os.system("say '%s'" % (message))
 
@@ -9,29 +28,14 @@ def say_and_print(message):
     say(message)
 
 def get_contextual_reply(message):
-    l = [
-        ('james', 'What is your favorite thing about James?'),
-        ('mommy', 'What color does mommy like the best?'),
-        ('daddy', 'What food does daddy like to eat the most?'),
-        ('no', 'Tell me.  Why not?'),
-        ('you', 'We are talking about you. Not me.'),
-        ('I', 'Do you always talk about yourself so much?'),
-        ('me', 'Do you always talk about yourself so much?')
-    ]
-    for pattern, reply in l:
+    for pattern, reply in responses:
         if message.find(pattern) != -1:
             return reply
     return ''
 
 def get_random_reply():
-    l = [
-        'Does my head smells like poop?',
-        'Anyone like tennis?',
-        'Is Mommy is a silly goose',
-        'What do you want to talk about?',
-        'What is your favorite game?'
-        'What movie do you like the most?']
-    return l[random.randint(0, len(l) - 1)]
+    return random_responses[
+        random.randint(0, len(random_responses) - 1)]
 
 def get_reply(message):
     return  get_contextual_reply(message) or get_random_reply()
